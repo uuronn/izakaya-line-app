e2d3ffd97b2b37b0221be98158a15b9c1b7dd3e9
uuronn <uuronprogram@gmail.com>
2023-08-06 07:56:12 +0900

    orderList

--- src/funcs/line-bot/usecases/messages/text.ts
+++ src/funcs/line-bot/usecases/messages/text.ts
@@ -1,7 +1,8 @@
 import { MessageEvent, TextEventMessage } from '@line/bot-sdk'
 
 import { lineClient } from '~/clients/line.client'
-import { makeReplyMessage } from '~/utils/line.util'
+import { db } from '~/libs/firebase/app'
+import { orderMessage } from '~/utils/order'
 import { errorLogger } from '~/utils/util'
 
 // *********
@@ -10,9 +11,25 @@ import { errorLogger } from '~/utils/util'
 
 export const messageTextUsecase = async (event: MessageEvent): Promise<void> => {
   try {
-    const { text } = event.message as TextEventMessage
+    if (event.source.type === 'group') {
+      const { text } = event.message as TextEventMessage
 
-    await lineClient.replyMessage(event.replyToken, makeReplyMessage(text))
+      const groupId = event.source.groupId
+
+      const cityRef = db.collection('groupList').doc(groupId)
+      const doc = await cityRef.get()
+      const res = doc.data()
+
+      if (!doc.exists || !res) return
+
+      const list = res.orderList as { name: string; length: number }[]
+
+      list.push({ name: text, length: 1 })
+
+      await cityRef.set({ orderList: list })
+
+      await lineClient.replyMessage(event.replyToken, orderMessage(text, groupId, list))
+    }
   } catch (err) {
     errorLogger(err)
     throw new Error('message text Usecase')
--- src/utils/order.ts
+++ src/utils/order.ts
@@ -1,6 +1,60 @@
-import { Message } from '@line/bot-sdk'
+import { FlexComponent, Message } from '@line/bot-sdk'
 
-export const orderMessage = (text: string): Message => {
+export const orderMessage = (
+  text: string,
+  groupId: string,
+  list: { name: string; length: number }[]
+): Message => {
+  const orderBox = (list: { name: string; length: number }[]): FlexComponent[] => {
+    return list.map((item) => {
+      return {
+        type: 'box',
+        layout: 'horizontal',
+        borderWidth: '1px',
+        backgroundColor: '#B7A42DFF',
+        borderColor: '#100808FF',
+        contents: [
+          {
+            type: 'text',
+            text: item.name,
+            weight: 'bold',
+            flex: 50,
+            align: 'center',
+            gravity: 'center'
+          },
+          {
+            type: 'text',
+            text: `× ${item.length}`,
+            weight: 'bold',
+            flex: 14,
+            align: 'center',
+            gravity: 'center'
+          },
+          {
+            type: 'button',
+            action: {
+              type: 'message',
+              label: '＋',
+              text: '追加'
+            },
+            flex: 18,
+            style: 'primary'
+          },
+          {
+            type: 'button',
+            action: {
+              type: 'message',
+              label: 'ー',
+              text: '削除'
+            },
+            flex: 18,
+            style: 'secondary'
+          }
+        ]
+      }
+    })
+  }
+
   return {
     type: 'flex',
     altText: 'string',
@@ -23,145 +77,7 @@ export const orderMessage = (text: string): Message => {
         type: 'box',
         layout: 'vertical',
         spacing: 'md',
-        contents: [
-          {
-            type: 'box',
-            layout: 'horizontal',
-            borderWidth: '1px',
-            backgroundColor: '#B7A42DFF',
-            borderColor: '#100808FF',
-            contents: [
-              {
-                type: 'text',
-                text: text,
-                weight: 'bold',
-                flex: 50,
-                align: 'center',
-                gravity: 'center'
-              },
-              {
-                type: 'text',
-                text: '× 3',
-                weight: 'bold',
-                flex: 14,
-                align: 'center',
-                gravity: 'center'
-              },
-              {
-                type: 'button',
-                action: {
-                  type: 'message',
-                  label: '＋',
-                  text: '追加'
-                },
-                flex: 18,
-                style: 'primary'
-              },
-              {
-                type: 'button',
-                action: {
-                  type: 'message',
-                  label: 'ー',
-                  text: '削除'
-                },
-                flex: 18,
-                style: 'secondary'
-              }
-            ]
-          },
-          {
-            type: 'box',
-            layout: 'horizontal',
-            position: 'relative',
-            borderWidth: '1px',
-            backgroundColor: '#CE7777FF',
-            borderColor: '#0C0707FF',
-            contents: [
-              {
-                type: 'text',
-                text: 'もも串　タレ',
-                weight: 'bold',
-                flex: 50,
-                align: 'center',
-                gravity: 'center'
-              },
-              {
-                type: 'text',
-                text: '× 1',
-                weight: 'bold',
-                flex: 14,
-                align: 'center',
-                gravity: 'center'
-              },
-              {
-                type: 'button',
-                action: {
-                  type: 'message',
-                  label: '＋',
-                  text: '追加'
-                },
-                flex: 18,
-                style: 'primary'
-              },
-              {
-                type: 'button',
-                action: {
-                  type: 'message',
-                  label: 'ー',
-                  text: '削除'
-                },
-                flex: 18,
-                margin: 'none',
-                style: 'secondary'
-              }
-            ]
-          },
-          {
-            type: 'box',
-            layout: 'horizontal',
-            borderWidth: '1px',
-            backgroundColor: '#BA4040FF',
-            borderColor: '#0E0808FF',
-            contents: [
-              {
-                type: 'text',
-                text: '砂ずり',
-                weight: 'bold',
-                flex: 50,
-                align: 'center',
-                gravity: 'center'
-              },
-              {
-                type: 'text',
-                text: '× 22',
-                weight: 'bold',
-                flex: 14,
-                align: 'center',
-                gravity: 'center'
-              },
-              {
-                type: 'button',
-                action: {
-                  type: 'message',
-                  label: '＋',
-                  text: '追加'
-                },
-                flex: 18,
-                style: 'primary'
-              },
-              {
-                type: 'button',
-                action: {
-                  type: 'message',
-                  label: 'ー',
-                  text: '削除'
-                },
-                flex: 18,
-                style: 'secondary'
-              }
-            ]
-          }
-        ]
+        contents: orderBox(list)
       }
     }
   }
